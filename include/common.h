8a78d707d4dcea5000556e728f23c9722f9ba857
Theo Fabi <fabi.theo@gmail.com>
2023-10-07 15:00:55 -0400

    remove unused CHECK_EQUAL_.. macros

--- include/common.h
+++ include/common.h
@@ -43,22 +43,4 @@
  *  const char* A[] = { "l", "l", "l" };
  */
 
-#define CHECK_EQUAL_0_0 PROBE(~)
-#define CHECK_EQUAL_1_1 PROBE(~)
-#define CHECK_EQUAL_2_2 PROBE(~)
-#define CHECK_EQUAL_3_3 PROBE(~)
-#define CHECK_EQUAL_4_4 PROBE(~)
-#define CHECK_EQUAL_5_5 PROBE(~)
-#define CHECK_EQUAL_6_6 PROBE(~)
-#define CHECK_EQUAL_7_7 PROBE(~)
-#define CHECK_EQUAL_8_8 PROBE(~)
-#define CHECK_EQUAL_9_9 PROBE(~)
-
-#define CHECK_NUM_ARGS(n, ...)                                                 \
-    CHECK(CAT(CAT(CHECK_EQUAL_, n), CAT(_, PP_NARG(__VA_ARGS__))))
-
-#define CHECK_LIST_NUM_ARGS(n, list) CHECK_NUM_ARGS(n, EXPAND list)
-#define CHECK_LAMBDA_NUM_ARGS(n, ...)                                          \
-    CHECK_NUM_ARGS(n, LAMBDA_GET_ARGS(__VA_ARGS__))
-
 #endif // INCLUDE_COMMON_H_
