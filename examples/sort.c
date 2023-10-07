e03ea3be8edcfdc41d68369dc4471a0bf33c1f8b
Theo Fabi <fabi.theo@gmail.com>
2023-10-07 17:39:00 -0400

    rework sort.c example

--- examples/sort.c
+++ examples/sort.c
@@ -7,21 +7,20 @@
 #define USE_LOCAL_LABELS 1
 #include "../include/lambda.h"
 
-__attribute__((always_inline))
-__attribute__((unused))
+typedef union {
+    uint64_t * restrict u64;
+    uint32_t * restrict u32;
+    uint16_t * restrict u16;
+    uint8_t  * restrict u8;
+} _private_anyptr_t;
+
 static inline void ptrswap(
     register unsigned char * restrict a,
     register unsigned char * restrict b,
     int size
 ) {
-    typedef union {
-        uint64_t * restrict u64;
-        uint32_t * restrict u32;
-        uint16_t * restrict u16;
-        uint8_t  * restrict u8;
-    } _anyptr;
-    register _anyptr a_ptr = { .u8 = a };
-    register _anyptr b_ptr = { .u8 = b };
+    register _private_anyptr_t a_ptr = { .u8 = a };
+    register _private_anyptr_t b_ptr = { .u8 = b };
     register uint64_t temp;
 #define SW(n)   temp = *a_ptr.u ## n;                                       \
                 *a_ptr.u ## n++ = *b_ptr.u ## n;                            \
@@ -29,14 +28,14 @@ static inline void ptrswap(
     register int n = (size + 7) >> 3;
     switch (size & 0b111) {
         case 0: do {    SW(64); continue;
-        case 7:         SW(8);
-        case 6:         SW(16); goto _case4;
+        case 7:         SW(32); goto _case3;
+        case 6:         SW(32); goto _case2;
         case 5:         SW(8);
-        case 4: _case4: SW(32); continue;
-        case 3:         SW(8);
-        case 2:         SW(16); continue;
+        case 4:         SW(32); continue;
+        case 3: _case3: SW(8);
+        case 2: _case2: SW(16); continue;
         case 1:         SW(8);
-        } while(--n);
+                } while(--n);
     }
 #undef SW
 }
@@ -98,7 +97,6 @@ static inline void ptrswap(
 #define ARRAY_SIZE(a) (sizeof a / sizeof(*a))
 
 int main() {
-    printf("Hello, world!\n");
     int array[] = { 5, 4, 3, 2, 1, 999, -100, 0, 1, 1, 12 };
     for (int i = 0; i < ARRAY_SIZE(array); ++i) {
         printf("%d ", array[i]);
@@ -130,6 +128,33 @@ int main() {
     });
     for (int i = 0; i < ARRAY_SIZE(things); ++i) {
         printf("{%s %d} ", things[i].name, things[i].n);
+    }
+    putchar('\n');
+    putchar('\n');
+
+    char char_arr_arr[][5] = {
+        { 'A', 'a', 'b', 'c', '\0' },
+        { 'A', 'd', 'e', 'f', '\0' },
+        { 'A', 'z', '\0', '\0', '\0' },
+        { 'A', 'a', '\0', '\0', '\0' },
+    };
+    for (int i = 0; i < ARRAY_SIZE(char_arr_arr); ++i) {
+        putchar('[');
+        for (int j = 0; j < 5; ++j) {
+            printf(" %c", char_arr_arr[i][j]);
+        }
+        printf(" ] ");
+    }
+    putchar('\n');
+    ARRAY_SORT(char_arr_arr, ARRAY_SIZE(char_arr_arr), (char *a, char *b) {
+        yield(strcmp(a, b));
+    });
+    for (int i = 0; i < ARRAY_SIZE(char_arr_arr); ++i) {
+        putchar('[');
+        for (int j = 0; j < 5; ++j) {
+            printf(" %c", char_arr_arr[i][j]);
+        }
+        printf(" ] ");
     }
     putchar('\n');
 
